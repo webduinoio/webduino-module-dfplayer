8e1af78d1263b046075a1edfd66bb73580168b02
osk2 <sc60714@gmail.com>
2017-11-20 16:26:12 +0800

    Update strings

--- blockly/msg/blocks/zh-hans.js
+++ blockly/msg/blocks/zh-hans.js
@@ -1,12 +1,12 @@
 Blockly.Msg.WEBDUINO_DFPLAYER = 'MP3 ( DFPlayer )，Rx ';
 Blockly.Msg.WEBDUINO_DFPLAYER_TX = '  Tx';
-Blockly.Msg.WEBDUINO_DFPLAYER_PLAY = 'MP3 DFPlayer 播放曲目';
+Blockly.Msg.WEBDUINO_DFPLAYER_PLAY = '播放第';
 Blockly.Msg.WEBDUINO_DFPLAYER_START = '开始播放';
 Blockly.Msg.WEBDUINO_DFPLAYER_STOP = '停止播放';
 Blockly.Msg.WEBDUINO_DFPLAYER_PAUSE = '暂停';
-Blockly.Msg.WEBDUINO_DFPLAYER_LOOP = 'MP3 DFPlayer 重复播放';
+Blockly.Msg.WEBDUINO_DFPLAYER_LOOP = '重複播放第';
 Blockly.Msg.WEBDUINO_DFPLAYER_PREVIOUS = '播放上一首';
 Blockly.Msg.WEBDUINO_DFPLAYER_NEXT = '播放下一首';
-Blockly.Msg.WEBDUINO_DFPLAYER_VOLUME = 'MP3 DFPlayer 音量 (0~30)';
+Blockly.Msg.WEBDUINO_DFPLAYER_VOLUME = '设定音量 (0~30)';
 Blockly.Msg.WEBDUINO_DFPLAYER_SET = '设定';
 Blockly.Msg.WEBDUINO_DFPLAYER_NUM = '首';
