9cab93a7ec19e4efd4b15ba4ba27b50998cb6fa8
Marty <marty@kingkit101.com>
2018-01-31 09:46:08 +0800

    remove ack confirm function

--- DFPlayer.js
+++ DFPlayer.js
@@ -6,6 +6,7 @@
   }
 }(function (scope) {
   'use strict';
+  var SENSOR_DFPLAYER = 0x19;//dec:25
 
   var self;
   var proto;
@@ -27,17 +28,13 @@
     self = this;
     board.on(webduino.BoardEvent.SYSEX_MESSAGE,
       function (event) {
-        sendAndAckCount--;
         var m = event.message;
-        var resp = m[2];
-        sending = false;
-        if (waitAckAndSend.length > 0) {
-          var cmd = waitAckAndSend.shift();
-          self._board.send(cmd);
+        if (m[0] == 0x04 && m[1] == SENSOR_DFPLAYER) {
+          console.log("mp3 response:",m);
         }
       });
-    startQueue(board);
     self.init();
+    console.log("MP3 ready...");
   }
 
   DFPlayer.prototype = proto = Object.create(Module.prototype, {
@@ -55,72 +52,41 @@
   });
 
   proto.init = function () {
-    var cmd = [0xF0, 0x04, 0x19, 0x0 /*init*/ , this._rx, this._tx, 0xF7];
-    sendAndAckCount++;
+    var cmd = [0xF0, 0x04, SENSOR_DFPLAYER, 0x0 /*init*/ , this._rx, this._tx, 0xF7];
     this._board.send(cmd);
   }
 
   proto.play = function (num) {
-    var cmd = [0xF0, 0x04, 0x19, 0x01, num, 0xF7];
-    sendAndAckCount++;
-    waitAckAndSend.push(cmd);
+    var cmd = [0xF0, 0x04, SENSOR_DFPLAYER, 0x01, num, 0xF7];
+    this._board.send(cmd);
   }
 
   proto.start = function () {
-    sendAndAckCount++;
-    waitAckAndSend.push([0xF0, 0x04, 0x19, 0x02 /*Start*/ , 0xF7]);
+    this._board.send([0xF0, 0x04, SENSOR_DFPLAYER, 0x02 /*Start*/ , 0xF7]);
   }
 
   proto.stop = function () {
-    sendAndAckCount++;
-    waitAckAndSend.push([0xF0, 0x04, 0x19, 0x03 /*Stop*/ , 0xF7]);
+    this._board.send([0xF0, 0x04, SENSOR_DFPLAYER, 0x03 /*Stop*/ , 0xF7]);
   }
 
   proto.pause = function () {
-    sendAndAckCount++;
-    waitAckAndSend.push([0xF0, 0x04, 0x19, 0x04 /*Pause*/ , 0xF7]);
+    this._board.send([0xF0, 0x04, SENSOR_DFPLAYER, 0x04 /*Pause*/ , 0xF7]);
   }
 
   proto.volume = function (volume) {
-    sendAndAckCount++;
-    waitAckAndSend.push([0xF0, 0x04, 0x19, 0x05, volume, 0xF7]);
+    this._board.send([0xF0, 0x04, SENSOR_DFPLAYER, 0x05, volume, 0xF7]);
   }
 
   proto.previous = function () {
-    sendAndAckCount++;
-    waitAckAndSend.push([0xF0, 0x04, 0x19, 0x06 /*Previous*/ , 0xF7]);
+    this._board.send([0xF0, 0x04, SENSOR_DFPLAYER, 0x06 /*Previous*/ , 0xF7]);
   }
 
   proto.next = function () {
-    sendAndAckCount++;
-    waitAckAndSend.push([0xF0, 0x04, 0x19, 0x07 /*Next*/ , 0xF7]);
+    this._board.send([0xF0, 0x04, SENSOR_DFPLAYER, 0x07 /*Next*/ , 0xF7]);
   }
 
   proto.loop = function (num) {
-    sendAndAckCount++;
-    waitAckAndSend.push([0xF0, 0x04, 0x19, 0x08, num, 0xF7]);
+    this._board.send([0xF0, 0x04, SENSOR_DFPLAYER, 0x08, num, 0xF7]);
   }
-
-  function startQueue(board) {
-    setInterval(function () {
-      if (sendAndAckCount == waitAckAndSend.length && waitAckAndSend.length > 0) {
-        var cmd = waitAckAndSend.shift();
-        board.send(cmd);
-      }
-      if (sending || sendArray.length == 0) {
-        return;
-      }
-      sending = true;
-      var sendObj = sendArray.shift();
-      sendAck = sendObj.ack;
-      if (sendAck > 0) {
-        board.send(sendObj.obj);
-      } else {
-        sending = false;
-        sendCallback();
-      }
-    }, 0);
-  }
-
   scope.module.DFPlayer = DFPlayer;
 }));
